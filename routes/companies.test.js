bbc4d14683761e20f277c562713f089bbfa0a55f
jessicaelee <jessica.e.lee.93@gmail.com>
2020-02-13 16:50:55 -0800

    tests working

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -5,7 +5,7 @@ const db = require("../db");
 
 let testCompany;
 
-beforeEach(async function() {
+beforeEach(async function () {
     let result = await db.query(`
       INSERT INTO
         companies (code, name, description) VALUES ('testCode', 'testName', 'testDescription')
@@ -13,15 +13,28 @@ beforeEach(async function() {
     testCompany = result.rows[0];
 });
 
-describe("GET /companies", function(){
-    test("Gets a list of 1 company", async function(){
+//wants code + name
+describe("GET /companies", function () {
+    test("Gets a list of 1 company", async function () {
         const response = await request(app).get("/companies")
+        delete testCompany.description;
+        delete testCompany.invoices;
         expect(response.statusCode).toEqual(200);
-        // expect(response.body).toEqual({
-        //     companies: [testCompany]
-        // });
+        expect(response.body).toEqual({
+            "companies": [testCompany]
+        });
     })
 });
+
+describe("GET /companies/[code]", function () {
+    test("Get information on one company", async function () {
+        const response = await request(app).get(`/companies/${testCompany.code}`);
+        testCompany.invoices = []
+        console.log(testCompany)
+        expect(response.statusCode).toEqual(200);
+        expect(response.body).toEqual(testCompany);
+    })
+})
 
 
 
